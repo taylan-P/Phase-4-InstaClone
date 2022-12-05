eee0d646e642946bda20c2cd348a0b8415f11636
TaylanPostalci <tay.postal@gmail.com>
2022-12-05 10:51:10 -0500

    postCardInit

--- src/views/Feed.js
+++ src/views/Feed.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar"
+import FeedCard from "../components/Feed/FeedCard"
 import { useNavigate } from "react-router-dom";
 
 export default function Feed() {
@@ -9,6 +10,7 @@ export default function Feed() {
     return (
         <div>
             <Navbar />
+            <FeedCard />
 
         </div>
 
